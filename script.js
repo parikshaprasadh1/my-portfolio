66d961bb0badfb47606a84c4d159b489e3b33dad
parikshaprasadh1 <ep24b014@smail.iitm.ac.in>
2025-04-28 19:20:53 +0530

    adding transition

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-
+document.querySelectorAll('.link').forEach(function(link){
+link.addEventListener('click',function(e){
+e.preventDefault();
+document.body.classList.add('fade-out');
+setTimeout(function(){
+window.location.href = link.href;
+},500);
+});
+});
